f7a1b53f593f2b9cc5ba70246dcada9ab8d36644
Phanthro <matheusmarianomtd@gmail.com>
2025-03-16 20:17:24 -0300

    Arquivo referente a estudos de variáveis em C.

---
+++ n1_nc4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade;
+    float altura = 1.75;
+    double salario = 3000.50;
+    char opcao = 'S';
+    char nome[20] = "Matheus";
+}
