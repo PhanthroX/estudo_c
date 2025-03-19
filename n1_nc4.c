435464744e4cbf0e3238c542c6c756c2ffc69aed
matheus_mariano <matheusmarianomtd@gmail.com>
2025-03-19 12:48:31 -0300

    Iniciando entrada e saída de dados

--- n1_nc4.c
+++ n1_nc4.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-
+#include <locale.h>
 int main(){
-    int idade = 25;
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    int idade = 27;
     int quantidade;
     float altura = 1.75;
     double salario = 3000.50;
     char opcao = 'S';
     char nome[20] = "Matheus";
+
+    printf("A idade do %s: %d\n", nome, idade);
+    printf("A altura é: %e\n", altura);
+    printf("A opção é: %c\n", opcao);
+
+
+
+
+
 }
