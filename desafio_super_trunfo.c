2681374333684f3e7fbdbcafcc56a42391d714ef
Phanthro <matheusmarianomtd@gmail.com>
2025-03-31 20:37:39 -0300

    Adicionando cálculo de densidade populacional e pib percapta e exibindo em cada carta.

--- desafio_super_trunfo.c
+++ desafio_super_trunfo.c
@@ -8,28 +8,26 @@ int main() {
     char estado_a[3], estado_b[3], cidade_a[50], cidade_b[50];  
     int populacao_a, populacao_b, pontos_turisticos_a, pontos_turisticos_b;
     float area_a, area_b, pib_a, pib_b;
+    float densidade_populacional_a, densidade_populacional_b, pib_percapta_a, pib_percapta_b;
 
     /* Dados Primeira Carta */
     printf("Dados da Primeira Carta: \n");
 
-    // Captura corretamente a sigla do estado
     printf("Informe a sigla do estado: ");
     scanf("%2s", estado_a);
     getchar(); // Remove o ENTER do buffer
 
-    // Lendo uma string para o nome da cidade
     printf("Informe o nome da cidade: ");
     fgets(cidade_a, 50, stdin);
-    cidade_a[strcspn(cidade_a, "\n")] = '\0'; // Remove o ENTER da string
+    cidade_a[strcspn(cidade_a, "\n")] = '\0';
 
-    // Lendo os valores numéricos corretamente
-    printf("Informe a população da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe a população da cidade: ");
     scanf("%i", &populacao_a);
 
-    printf("Informe a área da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe a área da cidade: ");
     scanf("%f", &area_a);
 
-    printf("Informe o PIB da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe o PIB da cidade: ");
     scanf("%f", &pib_a);
 
     printf("Informe o número de pontos turísticos da cidade: ");
@@ -40,31 +38,34 @@ int main() {
     /* Dados Segunda Carta */
     printf("\nDados da Segunda Carta: \n");
 
-    // Captura corretamente a sigla do estado
     printf("Informe a sigla do estado: ");
     scanf("%2s", estado_b);
-    getchar(); // Remove o ENTER do buffer
+    getchar(); 
 
-    // Lendo uma string para o nome da cidade
     printf("Informe o nome da cidade: ");
     fgets(cidade_b, 50, stdin);
-    cidade_b[strcspn(cidade_b, "\n")] = '\0'; // Remove o ENTER da string
+    cidade_b[strcspn(cidade_b, "\n")] = '\0';
 
-    printf("Informe a população da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe a população da cidade: ");
     scanf("%i", &populacao_b);
 
-    printf("Informe a área da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe a área da cidade: ");
     scanf("%f", &area_b);
 
-    printf("Informe o PIB da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe o PIB da cidade: ");
     scanf("%f", &pib_b);
 
-    printf("Informe o número de pontos turísticos da cidade (Não utilize pontuação, apenas números): ");
+    printf("Informe o número de pontos turísticos da cidade: ");
     scanf("%i", &pontos_turisticos_b);
 
-    /* Exibindo os dados cadastrados */
+    /* Declarando os valores de densidade e pib após a coleta dos dados. 
+        Caso a variável seja declarada acima, a memória não armazenará as informações */
+    densidade_populacional_a = populacao_a / area_a;
+    densidade_populacional_b = populacao_b / area_b;
+    pib_percapta_a = pib_a / populacao_a;
+    pib_percapta_b = pib_b / populacao_b;
 
-    // Primeira Carta
+    /* Exibindo os dados cadastrados */
     printf("\nDados cadastrados Primeira Carta:\n");
     printf("Estado: %s\n", estado_a);
     printf("Cidade: %s\n", cidade_a);
@@ -72,8 +73,9 @@ int main() {
     printf("Área: %.2f km²\n", area_a);
     printf("PIB: %.2f\n", pib_a);
     printf("Pontos turísticos: %i\n", pontos_turisticos_a);
+    printf("Densidade populacional: %.2f habitantes/km²\n", densidade_populacional_a);
+    printf("PIB per capita: %.2f\n", pib_percapta_a);
 
-    // Segunda Carta
     printf("\nDados cadastrados Segunda Carta:\n");
     printf("Estado: %s\n", estado_b);
     printf("Cidade: %s\n", cidade_b);
@@ -81,6 +83,8 @@ int main() {
     printf("Área: %.2f km²\n", area_b);
     printf("PIB: %.2f\n", pib_b);
     printf("Pontos turísticos: %i\n", pontos_turisticos_b);
+    printf("Densidade populacional: %.2f habitantes/km²\n", densidade_populacional_b);
+    printf("PIB per capita: %.2f\n", pib_percapta_b);
 
     return 0;
 }
