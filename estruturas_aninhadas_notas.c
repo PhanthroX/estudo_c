744ff91d60c63981069226a1636c920c19cbd7e9
Matheus Mariano Rocha <matheusmarianomtd@gmail.com>
2025-05-25 11:14:24 -0300

    feature: desenvolvendo a lógica de estrutura aninhada, desta vez utilizando o sitema de notas para validar multiplas condições.

---
+++ estruturas_aninhadas_notas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main() {
+  int nota;
+
+  printf("Digite a sua idade: \n");
+  scanf("%d", &nota);
+
+  if (nota >= 90) {
+    printf("Sua nota é A\n");
+  } else if (nota >= 80) {
+    printf("Sua nota é B\n");
+  } else if (nota >= 70) {
+    printf("Sua nota é C\n");
+  } else if (nota >= 60) {
+    printf("Sua nota é D\n");
+  } else {
+    printf("Sua nota é F\n");
+  }
+
+  return 0;
+}
